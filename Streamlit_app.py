2ef9ff14a8681b586a22a63dfc78126d402629cc
SanskarKu <Sanskar.kumar.2773@gmail.com>
2025-12-07 15:15:38 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -3,7 +3,8 @@ import pandas as pd
 import pickle
 import pickle
 
-model_path = r"C:\Users\sansk\Documents\Project 2\heart_disease_model.pkl"
+model_path = "heart_disease_model.pkl"
+
 
 with open(model_path, "rb") as f:
     model = pickle.load(f)
